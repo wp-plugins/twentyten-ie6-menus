7a4bd8b4b0effe69fc37f81cde0a0052fda7687a
saracannon <saracannon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-26 14:38:39 +0000

    Initial Commit of the twentyten dropdown plugin
    git-svn-id: https://plugins.svn.wordpress.org/twentyten-ie6-menus/trunk@281130 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ twentyten_ie6.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Twenty Ten IE6 Menus
+ * Author: Sara Cannon
+ * Author URI: http://sara-cannon.com
+ * Description: Make the menu drop down in IE6 (if you care about that sort of thing)
+ * Version: 1.0
+ * License: GPL2
+ */
+function sara_twentyten_ie6_menus_enqueue() {
+    wp_enqueue_script( 'jquery' );
+}
+add_action( 'after_setup_theme', 'sara_twentyten_ie6_menus_enqueue' );
+
+function sara_twentyten_ie6_menus_script() {
+?>
+<!--[if lte IE 6]>
+<script type="text/javascript">
+jQuery(document).ready( function($) {
+    $('#access li').mouseover( function() {
+        $(this).find('ul').show();
+    });
+    $('#access li').mouseleave( function() {
+        $(this).find('ul').hide();
+    });
+    $('#access li ul').mouseleave( function() {
+        $(this).hide();
+    });
+});
+</script>
+<![endif]-->
+<?php
+}
+add_action( 'wp_footer', 'sara_twentyten_ie6_menus_script' ); 
